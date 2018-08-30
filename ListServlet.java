2455e3a5c646f0751cb89a4ee20b0c72b091fc07
zhongm15907006125 <40863737+zhongm15907006125@users.noreply.github.com>
2018-08-30 11:48:31 +0800

    Update ListServlet.java

--- ListServlet.java
+++ ListServlet.java
@@ -28,6 +28,8 @@ public class ListServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ProfileBiz profilebiz=new ProfileBizImpl();
+		List<Profile> profiles=profilebiz.getAllProfiles()
 	}
 
 }
